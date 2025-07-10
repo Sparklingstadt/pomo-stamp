65dfc353c35cfcfc7d9083b062f3f9655a8a1bff
Sparklingstadt <sparklingstadt@outlook.jp>
2025-07-10 11:50:40 +0900

    feat(api): add POST /api/pomodoro endpoint

--- app/api/pomodoro/route.ts
+++ app/api/pomodoro/route.ts
@@ -1,7 +1,26 @@
+import { pomodoroSchema } from "@/lib/schemas/pomodoro/schema";
 import { NextResponse } from "next/server";
 
 export async function GET() {
     return NextResponse.json({
         message: "200 OK"
     })
+}
+
+export async function POST(req: Request) {
+    const payload = await req.json()
+    const result = pomodoroSchema.safeParse(payload)
+
+    if(!result.success) {
+        return NextResponse.json({
+            message: "invalid data format",
+            payload,
+            status: 400
+        }, { status: 400 })
+    }
+
+    return NextResponse.json({
+        payload,
+        status: 200
+    }, { status: 200 })
 }
