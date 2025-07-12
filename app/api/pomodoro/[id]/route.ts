ed2648c83b1e9407ce6666f9ce51356ac53aadbb
Sparklingstadt <sparklingstadt@outlook.jp>
2025-07-12 11:15:09 +0900

    feat(api/pomodoro): implement GET handler for /api/pomodoro/[id]

--- app/api/pomodoro/[id]/route.ts
+++ app/api/pomodoro/[id]/route.ts
@@ -1,9 +1,11 @@
+import { mockPomodoros } from "@/lib/schemas/pomodoro/mock";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params } : { params: Promise<string>}){
-    return NextResponse.json({
-        status: 200,
-        message: "200 OK",
-        params
-    })
+export async function GET(req: Request, { params } : { params: { id: string }}){
+    const id = parseInt(params.id)
+    const pomodoro = mockPomodoros.find(item => item.id === id)
+
+    return pomodoro
+        ? NextResponse.json(pomodoro)
+        : NextResponse.json({ error: `id ${params.id} not found` }, { status: 404 })
 }
