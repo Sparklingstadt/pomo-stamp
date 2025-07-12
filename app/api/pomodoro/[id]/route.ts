e3e94d237b04220031254e640a024159ae4ac0c8
Sparklingstadt <sparklingstadt@outlook.jp>
2025-07-12 13:00:55 +0900

    feat(api/pomodoro): implement PUT handler

--- app/api/pomodoro/[id]/route.ts
+++ app/api/pomodoro/[id]/route.ts
@@ -1,4 +1,5 @@
 import { mockPomodoros } from "@/lib/schemas/pomodoro/mock";
+import { pomodoroSchema } from "@/lib/schemas/pomodoro/schema";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, { params } : { params: { id: string }}){
@@ -9,3 +10,16 @@ export async function GET(req: Request, { params } : { params: { id: string }}){
         ? NextResponse.json(pomodoro)
         : NextResponse.json({ error: `id ${params.id} not found` }, { status: 404 })
 }
+
+export async function PUT(req: Request, { params } : { params: { id: string }}) {
+    const body = await req.json()
+    const result = pomodoroSchema.safeParse(body)
+
+    if(!result.success) return NextResponse.json({ error: `400 Bad request`, body}, { status: 400 })
+
+    const id = parseInt(params.id)
+    const pomodoro = mockPomodoros.find(item => item.id === id)
+    if(!pomodoro) return NextResponse.json({ error: `id ${params.id} not found`}, { status: 404})
+
+    return NextResponse.json({ message: "200 Successfully updated!", body}, { status: 200 })
+} 
