66d65806658c16261bed65f12ef7765513d292ac
Sparklingstadt <sparklingstadt@outlook.jp>
2025-07-24 11:00:05 +0900

    feat(schema): add uuid field to pomodoro mock data

--- lib/schemas/pomodoro/mock.ts
+++ lib/schemas/pomodoro/mock.ts
@@ -3,6 +3,7 @@ import type { Pomodoro } from "./schema"
 export const mockPomodoros: Pomodoro[] = [
     {
         id: 1,
+        uuid: "d7b112fa-cf72-4520-b57d-a2ba444763b3",
         task: "見本ぽも",
         date: {
             month: 7,
@@ -11,6 +12,7 @@ export const mockPomodoros: Pomodoro[] = [
     },
     {
         id: 2,
+        uuid: "97c8fe80-c43f-4659-8cbc-792288bdac9f",
         task: "見本ぽも",
         date: {
             month: 7,
