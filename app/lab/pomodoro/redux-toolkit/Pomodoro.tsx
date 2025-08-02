a22508394bdada3248638e066513f895aaf334dc
Sparklingstadt <sparklingstadt@outlook.jp>
2025-08-02 21:33:33 +0900

    feat: counter with RTK

--- app/lab/pomodoro/redux-toolkit/Pomodoro.tsx
+++ app/lab/pomodoro/redux-toolkit/Pomodoro.tsx
@@ -1,5 +1,16 @@
-export default function Pomodoro() {
+'use client'
+
+import { Provider } from "react-redux";
+import Counter from "./Counter";
+import { store } from "./store";
+
+export default function Pomodoro(){
   return (
-    <div>Pomodoro</div>
+    <div>
+      <h1>Pomodoro</h1>
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    </div>
   )
 }
