ead9b5d99524e28bbedd01297be966f1e4384ced
Sparklingstadt <sparklingstadt@outlook.jp>
2025-08-30 14:56:41 +0900

    feat(button): replace native button into Button component

--- app/components/PomodoroTableDataRow.tsx
+++ app/components/PomodoroTableDataRow.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Pomodoro } from "@/lib/schemas/pomodoro/schema";
 import Link from "next/link";
+import Button from "./Button";
 
 export default function PomodoroTableDataRow({ pomodoro }: { pomodoro: Pomodoro}){
   const handleDeletePomodoro = async (id: number) => {
@@ -16,8 +17,10 @@ export default function PomodoroTableDataRow({ pomodoro }: { pomodoro: Pomodoro}
       <td className="px-4 py-2">{ pomodoro.task }</td>
       <td className="px-4 py-2">{ pomodoro.memo }</td>
       <td className="px-4 py-2">
-        <Link className="inline-block px-4 py-1 text-sm bg-blue-500 text-white" href={`/edit/${pomodoro.id}`}>編集</Link>
-        <button className="px-4 py-1 text-sm bg-blue-500 text-white" onClick={() => handleDeletePomodoro(pomodoro.id)}>削除</button>
+        <Button>
+          <Link href={`/edit/${pomodoro.id}`}>編集</Link>
+        </Button>
+        <Button onClick={() => handleDeletePomodoro(pomodoro.id)}>削除</Button>
       </td>
     </tr>
   )
