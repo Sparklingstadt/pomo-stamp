4a6d15a8e1d2ba3d023cdb692052dd384c4e7403
Sparklingstadt <sparklingstadt@outlook.jp>
2025-08-30 15:33:44 +0900

    feat(button): replace to shadcn/ui button component

--- app/components/PomodoroTableDataRow.tsx
+++ app/components/PomodoroTableDataRow.tsx
@@ -1,7 +1,7 @@
 'use client'
+import { Button } from "@/components/ui/button";
 import { Pomodoro } from "@/lib/schemas/pomodoro/schema";
 import Link from "next/link";
-import Button from "./Button";
 
 export default function PomodoroTableDataRow({ pomodoro }: { pomodoro: Pomodoro}){
   const handleDeletePomodoro = async (id: number) => {
@@ -17,10 +17,10 @@ export default function PomodoroTableDataRow({ pomodoro }: { pomodoro: Pomodoro}
       <td className="px-4 py-2">{ pomodoro.task }</td>
       <td className="px-4 py-2">{ pomodoro.memo }</td>
       <td className="px-4 py-2">
-        <Button>
+        <Button className="bg-blue-500 text-white">
           <Link href={`/edit/${pomodoro.id}`}>編集</Link>
         </Button>
-        <Button onClick={() => handleDeletePomodoro(pomodoro.id)}>削除</Button>
+        <Button className="bg-red-500 text-white" onClick={() => handleDeletePomodoro(pomodoro.id)}>削除</Button>
       </td>
     </tr>
   )
