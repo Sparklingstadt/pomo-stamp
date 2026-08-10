8bc0fec1c5e3b5dfefdb510f545f172f3ad0a485
Sparklingstadt <sparklingstadt@outlook.jp>
2026-08-11 05:39:22 +0900

    fix: improve form accessibility (#22)

--- app/components/InputTextField.tsx
+++ app/components/InputTextField.tsx
@@ -1,9 +1,10 @@
-export default function InputTextField({ value, onChange, placeholder }: {
+export default function InputTextField({ id, value, onChange, placeholder }: {
+  id: string;
   value: string;
   placeholder: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) {
   return (
-    <input className="text-small border border-gray-300 px-4 py-1 outline:outline" value={value} onChange={onChange} placeholder={placeholder} />
+    <input id={id} className="text-small border border-gray-300 px-4 py-1 outline:outline" value={value} onChange={onChange} placeholder={placeholder} />
   );
 }
